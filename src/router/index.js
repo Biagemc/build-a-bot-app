ebc7ce9376a3ae36e235b3811c2f7032ff47021f
biagemc <biagemc@gmail.com>
2020-08-08 21:09:31 -0400

    added vertical sidebar

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,13 @@ import Router from "vue-router";
 import HomePage from "../home/HomePage.vue";
 import RobotBuilder from "../build/RobotBuilder.vue";
 import PartInfo from "../parts/PartInfo.vue";
+import BrowseParts from "../parts/BrowseParts.vue";
+import RobotHeads from "../parts/RobotHeads.vue";
+import RobotTorsos from "../parts/RobotTorsos.vue";
+import RobotBases from "../parts/RobotBases.vue";
+import RobotArms from "../parts/RobotArms.vue";
+import SidebarStandard from "../sidebars/SidebarStandard.vue";
+import SidebarBuild from "../sidebars/SidebarBuild.vue";
 
 Vue.use(Router);
 
@@ -11,17 +18,51 @@ export default new Router({
     {
       path: "/",
       name: "Home",
-      component: HomePage,
+      components: {
+        default: HomePage,
+        sidebar: SidebarStandard,
+      },
     },
     {
       path: "/build",
       name: "Build",
-      component: RobotBuilder,
+      components: {
+        default: RobotBuilder,
+        sidebar: SidebarBuild,
+      },
+    },
+    {
+      path: "/parts/browse",
+      name: "BrowseParts",
+      component: BrowseParts,
+      children: [
+        {
+          name: "BrowseHeads",
+          path: "heads",
+          component: RobotHeads,
+        },
+        {
+          name: "BrowseArms",
+          path: "arms",
+          component: RobotArms,
+        },
+        {
+          name: "BrowseTorsos",
+          path: "torsos",
+          component: RobotTorsos,
+        },
+        {
+          name: "BrowseBases",
+          path: "bases",
+          component: RobotBases,
+        },
+      ],
     },
     {
       path: "/parts/:partType/:id",
       name: "Parts",
       component: PartInfo,
+      props: true,
     },
   ],
 });
