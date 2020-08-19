2081449074a84d0d2aa8306cbc0f8a84ce34faf8
biagemc <biagemc@gmail.com>
2020-08-18 22:21:42 -0400

    added vuex store and  cart state

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,14 @@
               Build
             </router-link>
           </li>
+          <li class="nav-item cart">
+            <router-link class="nav-link" to="/cart" exact>
+              Cart
+            </router-link>
+            <div class="cart-items">
+              {{ cart.length }}
+            </div>
+          </li>
         </ul>
       </nav>
     </header>
@@ -33,6 +41,11 @@
 
 export default {
   name: "App",
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
 };
 </script>
 
@@ -80,6 +93,11 @@ ul {
 .router-link-active {
   color: white;
 }
+.nav-item.cart {
+  position: relative;
+  margin-left: auto;
+  border-right: none;
+}
 
 .container {
   display: flex;
@@ -92,5 +110,17 @@ ul {
   background-color: #aaa;
   width: 100px;
   height: 300px;
+}
+
+.cart-items {
+  position: absolute;
+  top: -5px;
+  right: -9px;
+  font-size: 18px;
+  width: 20px;
+  text-align: center;
+  display: inline-block;
+  border-radius: 100px;
+  background-color: mediumseagreen;
 }
 </style>
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -32,23 +32,6 @@
         @partSelected="part => (selectedRobot.base = part)"
       />
     </div>
-    <div>
-      <h1>Cart</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Robot</th>
-            <th class="cost">Cost</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr :key="index" v-for="(robot, index) in cart">
-            <td>{{ robot.head.title }}</td>
-            <td>{{ robot.cost }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
   </div>
 </template>
 
@@ -94,7 +77,7 @@ export default {
     addToCart() {
       const robot = this.selectedRobot;
       const cost = robot.head.cost + robot.leftArm.cost + robot.torso.cost + robot.rightArm.cost + robot.base.cost;
-      this.cart.push(Object.assign({}, robot, { cost }));
+      this.$store.dispatch("addRobotToCart", Object.assign({}, robot, { cost })).then(() => this.$router.push("/cart"));
       this.addedToCart = true;
     },
   },
@@ -211,17 +194,6 @@ export default {
   width: 220px;
   padding: 3px;
   font-size: 16px;
-}
-
-td,
-th {
-  text-align: left;
-  padding: 5px;
-  padding-right: 20px;
-}
-
-.cost {
-  text-align: right;
 }
 
 .sale-border {
